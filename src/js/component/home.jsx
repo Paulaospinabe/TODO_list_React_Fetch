ae13f0ce364086137ed540729072fdee28eb5588
Paulaospinabe <ospinapaula6.1@gmail.com>
2022-03-09 23:57:35 +0000

    TODO List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [nota, setNota] = useState("Recordar");
+	const [lista, setLista] = useState([]);
+	function eliminartarea(posicion) {
+		console.log(posicion);
+		let listafiltrada = lista.filter((item, index) => {
+			if (index !== posicion) {
+				return item;
+			}
+		});
+		setLista(listafiltrada);
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5">
+			<input
+				type="text"
+				placeholder="
+			Ingrese Nota"
+				onChange={(e) => setNota(e.target.value)}></input>
+			<ul>
+				{lista.map((notas, i) => {
+					return (
+						<div key={i}>
+							<li className="list-group-item ">{notas}</li>
+							<button
+								className="btn btn-warning"
+								onClick={() => eliminartarea(i)}>
+								X
+							</button>
+						</div>
+					);
+				})}
+			</ul>
+
+			<button
+				className="btn btn-success"
+				onClick={() => setLista([...lista, nota])}>
+				Añadir
+			</button>
 		</div>
 	);
 };
