a9410d017c098d7ab740acb11c1dd48266ae3aea
Paulaospinabe <ospinapaula6.1@gmail.com>
2022-03-22 21:29:23 +0000

    TODO list fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,42 +8,124 @@ const Home = () => {
 	const [nota, setNota] = useState("Recordar");
 	const [lista, setLista] = useState([]);
 	function eliminartarea(posicion) {
-		console.log(posicion);
 		let listafiltrada = lista.filter((item, index) => {
 			if (index !== posicion) {
 				return item;
 			}
 		});
 		setLista(listafiltrada);
+		console.log(listafiltrada);
+	}
+	function agrgartarea() {
+		let nuevalista = [...lista, { label: nota, done: false }];
+		setLista(nuevalista);
+		console.log(nuevalista);
+		fetch(url, {
+			method: "PUT",
+			body: JSON.stringify(nuevalista),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => {
+				console.log(res);
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch();
 	}
 
-	return (
-		<div className="container mt-5">
-			<input
-				type="text"
-				placeholder="
-			Ingrese Nota"
-				onChange={(e) => setNota(e.target.value)}></input>
-			<ul>
-				{lista.map((notas, i) => {
-					return (
-						<div key={i}>
-							<li className="list-group-item ">{notas}</li>
-							<button
-								className="btn btn-warning"
-								onClick={() => eliminartarea(i)}>
-								X
-							</button>
-						</div>
-					);
-				})}
-			</ul>
+	const url =
+		"https://assets.breatheco.de/apis/fake/todos/user/Paulaospinabe";
+	useEffect(() => {
+		fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify([]),
+		})
+			.then((res) => {
+				console.log(res);
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch();
+		fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => {
+				console.log(res);
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+				setLista(data);
+			})
+			.catch();
+	}, []);
 
-			<button
-				className="btn btn-success"
-				onClick={() => setLista([...lista, nota])}>
-				Añadir
-			</button>
+	return (
+		<div style={{ background: "black" }}>
+			<div
+				className="container mt-5"
+				style={{
+					width: "40rem",
+					height: "35rem",
+					background: "white",
+				}}>
+				<div
+					className="container mt-5, text-muted"
+					style={{ textAlign: "center" }}>
+					<h1>
+						<p style={{ color: "pink" }}>TODOS</p>
+					</h1>
+				</div>
+				<div
+					className="container mt-4"
+					style={{
+						background: "white",
+						width: "30rem",
+					}}>
+					<input
+						style={{ textAlign: "center" }}
+						type="text"
+						placeholder="
+					What needs to be done?"
+						onChange={(e) => setNota(e.target.value)}></input>
+				</div>
+				<ul className="list-group">
+					{lista.map((notas, i) => {
+						return (
+							<div key={i}>
+								<li className="list-group-item ">
+									{notas.label}
+								</li>
+								<button
+									className="btn btn-light, list-group-item"
+									onClick={() => eliminartarea(i)}>
+									X
+								</button>
+							</div>
+						);
+					})}
+				</ul>
+
+				<button
+					className="btn btn-light"
+					onClick={() => {
+						agrgartarea();
+					}}>
+					Añadir
+				</button>
+			</div>
 		</div>
 	);
 };
